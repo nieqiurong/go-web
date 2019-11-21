75a4e5e18ddfde8bab90738bbbc24947fe5b5b20
聂秋秋 <nieqiurong@163.com>
2019-11-21 16:44:23 +0800

    蛋疼的json反序列化.

--- cache/user.go
+++ cache/user.go
@@ -7,13 +7,30 @@ import (
 	"time"
 )
 
-const cacheKey string = "user:cache"
+const (
+	cacheKey   string = "user:cache"
+	dateLayOut        = "2006-01-02 15:04:05"
+)
 
 type jsonTime time.Time
 
-func (this jsonTime) MarshalJSON() ([]byte, error) {
-	var stamp = fmt.Sprintf("\"%s\"", time.Time(this).Format("2006-01-02 15:04:05"))
+func (t jsonTime) MarshalJSON() ([]byte, error) {
+	var stamp = fmt.Sprintf("\"%s\"", time.Time(t).Format(dateLayOut))
 	return []byte(stamp), nil
+}
+
+func (t *jsonTime) UnmarshalJSON(value []byte) error {
+	dateStr := string(value)
+	if dateStr != "" && dateStr != "null" {
+		d, err := time.Parse(`"`+dateLayOut+`"`, dateStr)
+		if err != nil {
+			log.Println("json.unmarshalJSON failed , err :", err)
+			return err
+		}
+		*t = jsonTime(d)
+		return nil
+	}
+	return nil
 }
 
 type User struct {
@@ -24,26 +41,37 @@ type User struct {
 }
 
 func SaveUser(CmbUid string, WxName string) error {
-	b, e := client.HExists(cacheKey, CmbUid).Result()
-	if e != nil {
-		log.Println("get user cache fail :", e)
-		return e
+	// 这个方法只是用来模拟存数据库.
+	b, err := client.HExists(cacheKey, CmbUid).Result()
+	var user User
+	if err != nil {
+		log.Println("client.hexists failed , err :", err)
+		return err
 	}
 	if b {
-		//预留更新
+		v, err := client.HGet(cacheKey, CmbUid).Result()
+		if err != nil {
+			log.Println("client.hget failed , err :", err)
+			return err
+		}
+		err = json.Unmarshal([]byte(v), &user)
+		if err != nil {
+			fmt.Println("json.unmarshal failed, err:", err)
+			return err
+		}
 	} else {
-		user := User{
+		user = User{
 			CmbUid:      CmbUid,
 			WxName:      WxName,
 			LoginTime:   jsonTime(time.Now()),
 			CreatedTime: jsonTime(time.Now()),
 		}
-		data, err := json.Marshal(user)
-		if err != nil {
-			fmt.Println("json.marshal failed, err:", err)
-			return nil
-		}
-		client.HSet(cacheKey, CmbUid, data)
 	}
+	data, err := json.Marshal(user)
+	if err != nil {
+		fmt.Println("json.marshal failed, err:", err)
+		return err
+	}
+	client.HSet(cacheKey, CmbUid, data)
 	return nil
 }
